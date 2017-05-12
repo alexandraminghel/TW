29e0d4af3a1d99e92d128ba53a7e990a7cef4dcf
alexandraminghel <alexandraminghel@gmail.com>
2017-05-12 12:18:55 +0300

    Pagina login
    Am modifiat pagina de login sa fie html si php ul separate, ca sa fie
    mvc

--- loginpage.php
+++ loginpage.php
@@ -1,58 +1,28 @@
 <?php
 session_start();
-
-if(isset($_POST['buton-login']))
-{
-	require ('connection.php');
-	$Email=$_POST["email"];
-	$Password=$_POST["password"];
+require ('connection.php');
+$Email=$_POST["email"];
+$Password=$_POST["password"];
 	$sql="Select * from users where '$Email'=EMAIL and '$Password'=PAROLA";
+	$sql1="Select * from users where '$Email'=EMAIL";
 	$result=mysqli_query($conn,$sql) or die(mysqli_error($conn));
+	$result1=mysqli_query($conn,$sql1) or die(mysqli_error($conn));
+	$count1 = mysqli_num_rows($result1);
 	$count = mysqli_num_rows($result);
+	if($count1!=1)
+		header('Location:loginpage_error_email.html');
+	else
+	{
 	if ($count== 1)
 {
 	$_SESSION['email']=$Email;
-	header('Location: newvisit.php');
+	header('Location: newvisit.html');
 }
 
 else
-	header('Location: loginpage_error.php');
-
+	header('Location: loginpage_error.html');
 
 }
 
+
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Login Page</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="loginpagestyle.css">
-	
-</head>
-<body>
-
-
-	<!--<button class="butoane1">Creare cont</button>
-	<button class="butoane2">Date de contact</button>-->
-
-</div>
-<form method="post" action="">
-<div class="wrap" >
-
-	<h2>
-		Login 
-	</h2>
-	<input type="text" name="email" placeholder="Your email">
-	<input type="password" name="password" placeholder="Your password">
-	<button class="btn-login" name="buton-login">Login</button>
-	<button class="btn-c"> <a href="createaccount.html">Creare cont</a></button>
-</div>
-</form>
-<div class="detalii">Vrei sa afli mai multe detalii? Apasa <a href="Harta.html"><ins>aici</ins></a> .</div>
-
-
-
-</body>
-</html>
