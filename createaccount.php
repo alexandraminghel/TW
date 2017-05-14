c1963dac6add73ff05ba4fb5ba45397b44fff93f
alexandraminghel <alexandraminghel@gmail.com>
2017-05-14 13:31:39 +0300

    Login page & Create Acount Page
    Am adaugat pentru parola sa fie hash uita din motive de securitate si am
    mai validat datele pentru login si creare cont

---
+++ createaccount.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+require ('connection.php');
+$Poza=NULL;
+$Nume=$_POST["nume"];
+$Prenume=$_POST["prenume"];
+$Email=$_POST["email"];
+$Parola1=$_POST["parola1"];
+$Parola2=$_POST["parola2"];
+$Telefon=$_POST["telefon"];
+
+$sql_cauta_email="select * from users where '$Email'=EMAIL";
+$result=mysqli_query($conn,$sql_cauta_email) or die(mysqli_error($conn));
+$count= mysqli_num_rows($result);
+
+
+if($Parola1==$Parola2 && $count==0 && filter_var($Email, FILTER_VALIDATE_EMAIL) && preg_match("/^0[0-9]{9}$/", $Telefon))
+{
+$Parola_securizata=password_hash($Parola1,PASSWORD_DEFAULT);
+$sql="insert into users(NUME,PRENUME,EMAIL,PAROLA,TELEFON,POZA) values ('$Nume', '$Prenume','$Email','$Parola_securizata','$Telefon','$Poza')";
+
+if(mysqli_query($conn, $sql)){
+    header('Location:loginpage.html');
+} 
+}
+else
+	if($Parola1!=$Parola2)
+	header('Location:createaccount_error_parole.html');
+	else
+		if($count==1)
+		header('Location:createaccount_error_email.html');
+	else
+		if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) 
+ 	header('Location:createaccount_error_emailvalid.html');
+ 	else
+ 		if(!preg_match("/^0[0-9]{9}$/", $Telefon))
+ 			header('Location:createaccount_error_phonevalid.html')
+
+?>
--- loginpage.php
+++ loginpage.php
@@ -3,26 +3,24 @@ session_start();
 require ('connection.php');
 $Email=$_POST["email"];
 $Password=$_POST["password"];
-	$sql="Select * from users where '$Email'=EMAIL and '$Password'=PAROLA";
 	$sql1="Select * from users where '$Email'=EMAIL";
-	$result=mysqli_query($conn,$sql) or die(mysqli_error($conn));
+	$sql2="Select parola from users where '$Email'=EMAIL";
 	$result1=mysqli_query($conn,$sql1) or die(mysqli_error($conn));
+	$result2=mysqli_query($conn,$sql2) or die(mysqli_error($conn));
 	$count1 = mysqli_num_rows($result1);
-	$count = mysqli_num_rows($result);
+	$count2 = mysqli_num_rows($result2);
+	$row=$result1->fetch_assoc();
+	$ceva=$row["PAROLA"];
+	$isPasswordCorrect = password_verify($Password, $ceva);
+
+	
 	if($count1!=1)
 		header('Location:loginpage_error_email.html');
 	else
-	{
-	if ($count== 1)
-{
-	$_SESSION['email']=$Email;
+	if ($isPasswordCorrect==1)
 	header('Location: newvisit.html');
-}
-
-else
+	else
 	header('Location: loginpage_error.html');
-
-}
-
+	
 
 ?>
